7c67ec9a63ee99ed11d367e5dea0058acc63de70
hysan110204@gmail.com <hysan110204@gmail.com>
2026-05-23 18:12:49 +0000

    Edited theme.css, theme/parameters.yaml and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,6 +5,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import m3.components as m3
+import time
 
 
 class Form1(Form1Template):
@@ -23,7 +24,18 @@ class Form1(Form1Template):
 
   
 
-
+  def display_pr(self,panel,length,gender,year):
+    start = time.time()
+    rows = app_tables.pr_table.search(tables.order_by("time_seconds"),Length=length,Gender=gender,Year=year)
+    int_time=time.time()
+    print(f"intermediate time {int_time-start}")
+    list_rows = []
+    for i, row in enumerate(rows):
+      row = dict(row)
+      row["Team Position"] = i + 1
+      list_rows.append(row)
+    panel.items = list_rows
+    print(f"final_time {time.time()-start}")
 
 
 
@@ -32,4 +44,6 @@ class Form1(Form1Template):
 
   @handle("refresh_button", "click")
   def refresh_button_click(self, **event):
-    self.repeating_panel_1.items = app_tables.pr_table.search(tables.order_by("time_seconds"),Length="1600 Meters")
+    self.display_pr(panel = self.repeating_panel_1, length = "800 Meters",gender = "Female",year = 2026)
+    self.display_pr(panel = self.repeating_panel_2, length = "1600 Meters",gender = "Female",year = 2026)
+    self.display_pr(panel = self.repeating_panel_4, length = "3200 Meters",gender = "Female",year = 2026)
