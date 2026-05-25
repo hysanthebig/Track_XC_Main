e48aa8f2fca7584051315041c3e89f75d839448c
hysan110204@gmail.com <hysan110204@gmail.com>
2026-05-25 04:14:35 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -123,14 +123,15 @@ class Form1(Form1Template):
     
     selected_gender = self.gender_button.text
     selected_sport = self.sport_button.text
-    if selected_sport 
-      selected_year = datetime.datetime.now().year
+
 
     if selected_sport == "Track":
+      selected_year = datetime.datetime.now().year
       self.display_pr(panel = self.repeating_panel_1, length = "800 Meters",gender = selected_gender,year = selected_year)
       self.display_pr(panel = self.repeating_panel_2, length = "1600 Meters",gender = selected_gender,year = selected_year)
       self.display_pr(panel = self.repeating_panel_3, length = "3200 Meters",gender = selected_gender,year = selected_year)
     else:
+      if 
       self.display_pr(panel = self.repeating_panel_1, length = "3.0",gender = selected_gender,year = selected_year)
       self.display_pr(panel = self.repeating_panel_2, length = "2.0",gender = selected_gender,year = selected_year)
       self.data_grid_4.visible = False
