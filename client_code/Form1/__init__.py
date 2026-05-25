4c7ef415b6aba141748ee35ddce70d16c6c95d5a
hysan110204@gmail.com <hysan110204@gmail.com>
2026-05-25 04:17:19 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -135,7 +135,7 @@ class Form1(Form1Template):
       if datetime.datetime.now().month < 6:
         selected_year = current_year-1
       else:
-        selected_year 
+        selected_year = current_year
       self.display_pr(panel = self.repeating_panel_1, length = "3.0",gender = selected_gender,year = selected_year)
       self.display_pr(panel = self.repeating_panel_2, length = "2.0",gender = selected_gender,year = selected_year)
       self.data_grid_4.visible = False
